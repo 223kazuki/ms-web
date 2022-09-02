ec8b787296bd965c96f74c32ece6b34a40285785
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2022-09-02 15:55:50 -0700

    Update members

--- src/cljs/ms_web/db.cljs
+++ src/cljs/ms_web/db.cljs
@@ -19,21 +19,21 @@
                            {:member/id 42
                             :member/name "青山遼太郎"
                             :member/image "aoyama.jpg"
-                            :member/introduction1 "工学部機械航空宇宙工学科\n出身：愛知県一宮市"
+                            :member/introduction1 "主将\n工学部機械航空宇宙工学科\n出身：愛知県一宮市"
                             :member/introduction2 "先輩のことが好き。"
                             :member/grade "4年"
                             :member/grade-id "grade4"}
                            {:member/id 43
                             :member/name "土屋英貴"
                             :member/image "tsuchiya.jpg"
-                            :member/introduction1 "工学部機械航空宇宙工学科\n出身：静岡県浜松市"
+                            :member/introduction1 "主務\n工学部機械航空宇宙工学科\n出身：静岡県浜松市"
                             :member/introduction2 "料理が好きで、いろいろな料理を作れる。"
                             :member/grade "4年"
                             :member/grade-id "grade4"}
                            {:member/id 44
                             :member/name "杉本空良"
                             :member/image "sugimoto.jpg"
-                            :member/introduction1 "教育学部\n出身: 静岡県浜松市"
+                            :member/introduction1 "副主将\n教育学部\n出身: 静岡県浜松市"
                             :member/introduction2 "ゲームが好き。オールジャンルでいける。"
                             :member/grade "3年"
                             :member/grade-id "grade3"}
@@ -44,6 +44,27 @@
                             :member/introduction2 "相撲歴10年のマネージャー。"
                             :member/grade "2年"
                             :member/grade-id "grade2"}
+                           {:member/id 48
+                            :member/name "アルベスネベスエゴンルカス"
+                            :member/image "egon.jpg"
+                            :member/introduction1 "人文学研究科言語学専攻\n研究生\n出身:ブラジル"
+                            :member/introduction2 "身長196cmの大型部員。"
+                            :member/grade "1年"
+                            :member/grade-id "grade1"}
+                           {:member/id 49
+                            :member/name "横井睦己"
+                            :member/image "yokoi.jpg"
+                            :member/introduction1 "理学部\n出身:愛知県一宮市"
+                            :member/introduction2 "ポケモン go で図鑑完成を目指す。"
+                            :member/grade "1年"
+                            :member/grade-id "grade1"}
+                           {:member/id 50
+                            :member/name "大西悠太"
+                            :member/image "onishi.jpg"
+                            :member/introduction1 "文学部\n出身:愛知県一宮市"
+                            :member/introduction2 "教科書書き込みガチ勢。"
+                            :member/grade "1年"
+                            :member/grade-id "grade1"}
                            {:member/id 1
                             :member/name "加藤 延夫"
                             :member/introduction1 "総長"
@@ -126,21 +147,21 @@
                            {:member/id 40
                             :member/name "尾田駿太"
                             :member/image "oda.jpg"
-                            :member/introduction1 "主将\n工学部物理工学科\n出身：愛知県名古屋市"
+                            :member/introduction1 "元主将\n工学部物理工学科\n出身：愛知県名古屋市"
                             :member/introduction2 "手足が長く、廻しを取るのが上手い。"
                             :member/grade "卒業生（2021年度卒）"
                             :member/grade-id "obg2021"}
                            {:member/id 39
                             :member/name "高橋純輝"
                             :member/image "takahashi.jpg"
-                            :member/introduction1 "副主将\n理学部物理学科\n出身：岐阜県岐阜市"
+                            :member/introduction1 "元副主将\n理学部物理学科\n出身：岐阜県岐阜市"
                             :member/introduction2 "真面目に見えて実はポンコツ。"
                             :member/grade "卒業生（2021年度卒）"
                             :member/grade-id "obg2021"}
                            {:member/id 38
                             :member/name "中川悠太"
                             :member/image "nakagawa.jpg"
-                            :member/introduction1 "主務\n農学部生物環境科学科\n出身：東京都世田谷区"
+                            :member/introduction1 "元主務\n農学部生物環境科学科\n出身：東京都世田谷区"
                             :member/introduction2 "麻雀好きのギャンブラー。"
                             :member/grade "卒業生（2021年度卒）"
                             :member/grade-id "obg2021"}
